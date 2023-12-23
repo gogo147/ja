814b4ff3401f8aa973ad6e878b296a4a3f1c18dc
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2023-12-24 00:17:38 +0100

    ca

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Img from './components/Img/Img';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
+      <Img />
       <Footer />
     </div>
   );
